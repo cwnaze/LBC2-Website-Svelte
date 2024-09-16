fec9338cd29ed1a7e3372a351aec5d0531e203c7
Casey Nazelrod <cwnaze@gmail.com>
2024-09-15 20:34:47 -0400

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      'yellow': {
+        100: '#FFE6AE',
+        200: '#F0A500',
+      },
+      'blue': {
+        'placeholder': '#33475690',
+        100: '#334756',
+        200: '#293241',
+      },
+      'error': '#FF0000',
+    },
+    borderWidth: {
+      default: '1px',
+        '0': '0',
+        '2': '2px',
+        '4': '4px',
+        '8': '8px',
+        'cursor': '25px'
+    },
+    fontFamily: {
+      sans: ['Ubuntu Mono', 'sans-serif']
+    },
+    extend: {
+      dropShadow: {
+        'dark': '0 3px 2px rgba(0, 0, 0, 0.25)',
+      }
+    },
   },
   plugins: [],
 }
