14c6c2f8c9da2f24aeafcbbae4661f9897f0d6fb
Casey Nazelrod <cwnaze@gmail.com>
2024-09-27 14:32:02 -0400

    init: details page

---
+++ src/routes/details/+page.svelte
@@ -0,0 +1,10 @@
+<h1>How Can I Get Involved</h1>
+<p>In a world that increasingly relies on technology for its day-to-day operations, cyber threats are more and more important to understand in the business world. The current landscape requires local family businesses to be aware of cyber threats just as much as the largest Fortune 500 companies in the ever-evolving cyber domain. The Loyola Blakefield Cyber Challenge aims to prepare the next generation regardless of career aspirations for the future landscape of business. Educating students on computer management and cyber threats is essential to future success in a workplace which will inevitably rely on understanding the technology which runs it.</p>
+<h2>Attendance</h2>
+<p>Attend the competition or the summit. Parents, when you drop off your student please join us for the summit to get insights into opportunities in the field.</p>
+<h2>Mentorship</h2>
+<p>Are you in the IT, Security, or Engineering fields? Volunteer to be a mentor for the day and be part of the teaching and learning experience.</p>
+<h2>Financial Support</h2>
+<p>Consider sponsoring the event. The goal is for this to be a free day for all participants.</p>
+<h2>Tell Others</h2>
+<p>Tell your friends, business associates, or anyone else that may be interested. Every business needs IT and cyber support from the small 5-employee company to the Fortune 500s.</p>
