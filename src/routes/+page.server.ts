27b00ccfd98710861c31f1e540f0c5b283a75446
Casey Nazelrod <cwnaze@gmail.com>
2024-10-16 12:03:38 -0400

    feat: newsletter functionality

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -10,6 +10,14 @@ export const actions: Actions = {
     register: async ({ cookies, request }) => {
         const data: FormData = await request.formData();
 
-        const email
+        const email: string = data.get('email')?.toString() ?? '';
+
+        if(email === '') return fail(400, {email_missing: true});
+        if(email.indexOf('@') === -1 || email.indexOf('.') === -1) return fail(400, {email, email_invalid: true});
+        const db: Connection = await ConnectDb();
+        const emails = await db.query("SELECT email FROM newsletter WHERE email = ?", [email]);
+        if (emails[0].toString().length > 0) return fail(400, {email, email_exists: true});
+
+        await db.query("INSERT INTO newsletter (email) VALUES (?)", [email]);
     }
 }
