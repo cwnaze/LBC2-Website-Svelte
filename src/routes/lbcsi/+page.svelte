e24c90e6de6987873105ed3a1eef29fb76c83734
Casey Nazelrod <cwnaze@gmail.com>
2024-10-15 22:57:54 -0400

    feat: lbcsi

---
+++ src/routes/lbcsi/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Navbar from "$lib/components/navbar.svelte";
+</script>
+
+<Navbar />
+<h1 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center">About LBCSI</h1>
+<p class="text-white text-2xl my-8 mx-20 text-center">Loyola Blakefield Cyber Security Informatics practices digital awareness and safety as well as the professional and ethical implementation of cybersecurity skills. It provides students with real world skills required by today’s fast-growing employers and offers on the most advanced opportunities to apply those skills in the classroom. This program further enables students to test drive their future careers by participating in many student-led projects, national competitions, and local internship opportunities. LBCSI fosters the talents and ambitions of its students, preparing them to excel in the professional field. Loyola Blakefield Cyber Security gives students opportunities to learn and grow on an unparalleled level.</p>
+<div class="bg-blue-100 flex flex-col items-center justify-center">
+    <img src="/images/wavesOpacity.svg" alt="divider" class="h-auto w-screen">
+    <p class="text-white text-xl mt-8 mb-4 mx-20 text-center">Check out our 10 years of LBCSI video!</p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/BsjYtXqf9QI?si=90blNkjEr9zm7VCt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <img src="/images/wavesOpacity.svg" alt="divider" class="h-auto w-screen rotate-180 mt-8">
+</div>
+
+<div class="flex mx-20 items-center justify-center">
+    <div>
+        <h2 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center">Legacy</h2>
+        <p class="text-white text-2xl my-8 mx-20 text-center">This year LBCSI celebrates its 11th year. The Loyola Blakefield Cyber initiative has given many students everlasting opportunities and memories, as well as expanding into a community of growth. With well over a hundred members, the club has continued the interest of cyber science through yearly outreach and rapid development. The leadership team’s hard work and commitment have kept production run smoothly, especially during Cyber Patriot and LBC2 season. This year marks the 7th LBC2 student run cyber competition.</p>
+    </div>
+    <img src="/images/lbcsiLogo.png" alt="legacy" class="h-auto w-screen">
+</div>
+<div class="flex mx-20 items-center justify-center">
+    <img src="/images/2023/image9.jpeg" alt="legacy" class="h-96 w-auto rounded mb-8">
+    <div>
+        <p class="text-white text-2xl my-8 mx-20 text-center">LBCSI is a student run initiative and directed by Steve Morrill, Head of Technology & Cyber Science at Loyola Blakefield. He has been at the school for seventeen years and started the club along with just a few other students back in 2011. With his dedication and mentorship, the club has expanded to what it is today, and without him, none of this would exist.</p>
+        <p class="text-white text-xl mt-2 text-center">Head of LBCSI - <a href="mailto:smorrill@loyolablakefield.org" class="underline">Steve Morrill</a></p>
+    </div>
+</div>
