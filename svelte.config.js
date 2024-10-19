43bd799d1e16ffe5839a70f4c0d61350cd97331a
Casey Nazelrod <cwnaze@gmail.com>
2024-10-19 13:01:56 -0400

    feat: update build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import fs from 'fs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: {
+	server: {
 		// vite: {
 		// 	optimizeDeps: {
 		// 	  include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
@@ -16,12 +17,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			https: {
-				key: 'key.pem',
-				cert: 'cert.pem'
-			}
-		})
+		https: {
+			key: fs.readFileSync('cert/key.pem'),
+			cert: fs.readFileSync('cert/cert.pem')
+		},
+		proxy: {}
 	}
 };
 
