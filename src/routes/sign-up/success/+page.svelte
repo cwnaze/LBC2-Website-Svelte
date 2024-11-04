46e99c89bf3eccbf619c51c25dfb51fd89096736
Emmett Omron <118745629+emomron2025@users.noreply.github.com>
2024-11-04 16:55:55 -0500

    style: improve formatting

--- src/routes/sign-up/success/+page.svelte
+++ src/routes/sign-up/success/+page.svelte
@@ -4,5 +4,5 @@
 
 <Navbar />
 
-<h1 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center">Registration Complete!!</h1>
+<h1 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center">Registration Complete!</h1>
 <img src="https://c.tenor.com/lVlfg-1BJ-IAAAAC/tenor.gif" alt="Success" class="mx-auto mt-8 w-1/2"/>
