e9dbaa58aae122d8583e7f36dc3c9180847d1c9d
Emmett Omron <118745629+emomron2025@users.noreply.github.com>
2024-11-04 17:21:00 -0500

    style: improve formatting, grammar

--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -4,28 +4,28 @@
 
 <Navbar/>
 <div class="my-8">
-    <h2 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center"> How Can I Get Involved</h2>
-    <p class="mx-20 text-center text-white text-xl mt-2">In a world that increasingly relies on technology for its day-to-day operations, cyber threats are more and more important to understand in the business world. The current landscape requires local family businesses to be aware of cyber threats just as much as the largest Fortune 500 companies in the ever-evolving cyber domain. The Loyola Blakefield Cyber Challenge aims to prepare the next generation regardless of career aspirations for the future landscape of business. Educating students on computer management and cyber threats is essential to future success in a workplace which will inevitably rely on understanding the technology which runs it.</p>
+    <h2 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center"> How Can I Get Involved?</h2>
+    <p class="mx-20 text-center text-white text-xl mt-2">In a world that increasingly relies on technology for its day-to-day operations, cyber threats are more and more important to understand in the business world. The current landscape requires local family businesses to be aware of cyber threats just as much as the largest Fortune 500 companies in the ever-evolving cyber domain. The Loyola Blakefield Cyber Challenge aims to prepare the next generation, regardless of career aspirations, for the future landscape of business. Educating students on computer management and cyber threats is essential to future success in a workplace which will inevitably rely on understanding the technology which runs it.</p>
     <h3 class="text-yellow-100 text-2xl font-bold mt-4 text-center">Attendance</h3>
-    <p class="mx-20 text-center text-white text-xl mt-2">Attend the competition or the summit. Parents, when you drop off your student please join us for the summit to get insights into opportunities in the field.</p>
+    <p class="mx-20 text-center text-white text-xl mt-2">Students are encouraged to attend the competition both to learn new skills and have fun. Experience level doesn't matter; we have divisions both for those completely new to cybersecurity and those with more experience. Parents, when you drop off your student, please join us for the parent track to get insights into opportunities with our program and in the field.</p>
     <h3 class="text-yellow-100 text-2xl font-bold mt-4 text-center">Mentorship</h3>
-    <p class="mx-20 text-center text-white text-xl mt-2">Are you in the IT, Security, or Engineering fields? Volunteer to be a mentor for the day and be part of the teaching and learning experience.</p>
+    <p class="mx-20 text-center text-white text-xl mt-2">Are you in the fields IT, security, or engineering? Volunteer to be a mentor for the day and be part of the teaching and learning experience. Email <a href="mailto:lbc2staff@gmail.com" class="underline hover:text-yellow-100">lbc2staff@gmail.com</a> for more details.</p>
     <h3 class="text-yellow-100 text-2xl font-bold mt-4 text-center">Financial Support</h3>
-    <p class="mx-20 text-center text-white text-xl mt-2">Consider sponsoring the event. The goal is for this to be a free day for all participants.</p>
+    <p class="mx-20 text-center text-white text-xl mt-2">Please consider sponsoring the event. Our competition is completely free for all participants, so we rely heavily on financial generosity to keep doing what we do. If you're interested in sponsoring LBC2, contact <a href="mailto:lbc2staff@gmail.com" class="underline hover:text-yellow-100">lbc2staff@gmail.com</a></p>
     <h3 class="text-yellow-100 text-2xl font-bold mt-4 text-center">Tell Others</h3>
-    <p class="mx-20 text-center text-white text-xl mt-2">Tell your friends, business associates, or anyone else that may be interested. Every business needs IT and cyber support from the small 5-employee company to the Fortune 500s.</p>
+    <p class="mx-20 text-center text-white text-xl mt-2">Tell your friends, business associates, or anyone else that may be interested about LBC2. Every business needs IT and cyber support, from the small five-employee company to the Fortune 500s.</p>
 </div>
 <div class="bg-blue-100">
     <img src="/images/wavesOpacity.svg" alt="divider" class="h-auto w-screen">
     <h2 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center">Things You Need To Know</h2>
     <h3 class="text-yellow-100 text-2xl font-bold mt-4 text-center">Parent Track</h3>
-    <p class="mx-20 text-center text-white text-xl mt-2">This year, the LBC2 competition day also featured a parent track for the parents and guardians of the competitors to learn what the competitors are learning by competing in LBC2, the importance of cybersecurity in today’s society, and how they can support their children in learning more about cybersecurity later on. The parent track was a huge success and will return next year.</p>
+    <p class="mx-20 text-center text-white text-xl mt-2">This year, the LBC2 competition day also featured a parent track for the parents and guardians of the competitors to learn what the competitors are learning by competing in LBC2, the importance of cybersecurity in today’s society, and how they can support their children in learning more about cybersecurity later on.</p>
     <h3 class="text-yellow-100 text-2xl font-bold mt-4 text-center">What You Need to Bring</h3>
     <ul class="mx-20 text-center text-white text-xl mt-2 mb-8">
         <li>Computer</li>
         <li>Charger</li>
         <li>Loaner computers will be available for beginner participants as needed</li>
-        <li>Kali Tools Are Reccomended</li>
+        <li>A <a href="kali.org" class="underline hover:text-yellow-100">Kali Linux</a> operating system or virtual machine is <b>strongly</b> recommended.</li>
     </ul>
     <img src="/images/wavesOpacity.svg" alt="divider" class="h-auto w-screen rotate-180">
 </div>
@@ -35,6 +35,6 @@
     </div>
     <div>
         <h2 class="text-yellow-200 text-4xl font-black m-8 text-center">IOT City</h2>
-        <p class=" text-center text-white text-xl mt-2 mb-8">The IoT (Internet of Things) City challenges were a resounding success. Consisting of six separate Capture the Flag style challenges with an IoT twist, ranging in difficulty from Intro to Advanced, the challenges tested competitors’ knowledge of both basic cybersecurity and IoT elements. The Internet of Things is an idea of how different appliances, or things, connect and communicate with each other. This can include your home security camera, phone, and even your toaster! This year, we featured a series of IOT-themed challenges revolving around common industrial “things”, such as traffic lights, billboards, and a water tower.</p>
+        <p class=" text-center text-white text-xl mt-2 mb-8">IoT (Internet of Things) is how different appliances, or things, connect and communicate with each other. This can include your home security camera, phone, and even your toaster! IoT is becoming increasingly relevant as more household devices become "smart", and will only grow more so. LBC2's IoT City challenges consist of six separate Capture the Flag-style challenges with an IoT twist. Ranging in difficulty from Intro to Advanced, the challenges test competitors’ knowledge of both basic cybersecurity and IoT elements. Last year, we featured a series of IOT-themed challenges revolving around common industrial “things”, such as traffic lights, billboards, and a water tower. We're working on new and exciting content to add to the city, so stay tuned!</p>
     </div>
 </div>
