bb0845428d82177e9acc070bfb3aa30a43e08af8
Emmett Omron <118745629+emomron2025@users.noreply.github.com>
2025-03-23 20:42:08 -0400

    chore: reword

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -8,4 +8,4 @@
 <Navbar />
 
 <h1 class="text-yellow-200 text-4xl font-black mt-8 mx-20 text-center">Registration is Closed!</h1>
-<p class="text-white text-xl mt-8 mb-4 mx-20 text-center">LBC2 registration is now closed. Thank you to everyone who signed up, and we hope to see you there!</p>
+<p class="text-white text-xl mt-8 mb-4 mx-20 text-center">LBC2 registration is now closed. Thank you to everyone who signed up, and we can't wait see you there!</p>
